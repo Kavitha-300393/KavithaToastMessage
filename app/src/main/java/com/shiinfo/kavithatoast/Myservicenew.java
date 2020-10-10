bd537739ba84c859246c4a738c04a9f5b0f97840
Kavitha <kavitha@cwd.co.in>
2020-10-10 13:20:43 -0700

    commit

---
+++ app/src/main/java/com/shiinfo/kavithatoast/Myservicenew.java
@@ -0,0 +1,225 @@
+package com.shiinfo.kavithatoast;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by kavitha on 1.04.2019.
+ */
+public class Myservicenew extends Service {
+
+    private static final String TAG_FOREGROUND_SERVICE = "FOREGROUND_SERVICE";
+
+    public static final String ACTION_START_FOREGROUND_SERVICE = "ACTION_START_FOREGROUND_SERVICE";
+
+    public static final String ACTION_STOP_FOREGROUND_SERVICE = "ACTION_STOP_FOREGROUND_SERVICE";
+
+    public static final String ACTION_PAUSE = "ACTION_PAUSE";
+
+    public static final String ACTION_PLAY = "ACTION_PLAY";
+
+    public static final long INTERVAL=60000;//variable for execute services every 1 minute
+    private Handler mHandler=new Handler(); // run on another Thread to avoid crash
+    private Timer mTimer=null; // timer handli
+    String currentDateandTime;
+    FileOutputStream fstream;
+
+    public Myservicenew() {
+    }
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG_FOREGROUND_SERVICE, "My foreground service onCreate().");
+        if(mTimer!=null)
+            mTimer.cancel();
+        else
+            mTimer=new Timer(); // recreate new timer
+        mTimer.scheduleAtFixedRate(new TimeDisplayTimerTask(),0,INTERVAL);// schedule task
+    }
+
+    private class TimeDisplayTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            // run on another thread
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    // display toast at every 1 minute
+                    Toast.makeText(getApplicationContext(), "Notify", Toast.LENGTH_SHORT).show();
+                    // display toast at every 1 minute
+                    try {
+                        ContextCompat.checkSelfPermission(getApplicationContext(),"WRITE_EXTERNAL_STORAGE");
+                        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                        File myFile = new File(folder,"user_details");
+                        fstream = new FileOutputStream(myFile);
+                        //  fstream.write(username.getBytes());
+                        fstream.write(currentDateandTime.getBytes());
+                        fstream.close();
+                        // Toast.makeText(getApplicationContext(), "Details Saved in "+myFile.getAbsolutePath(),Toast.LENGTH_SHORT).show();
+//                        intent = new Intent(WriteExternalStorage.this,DetailsActivity.class);
+//                        startActivity(intent);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null) {
+            String action = intent.getAction();
+            if(action!=null)
+
+                switch (action) {
+                    case ACTION_START_FOREGROUND_SERVICE:
+                        startForegroundService();
+                        Toast.makeText(getApplicationContext(), "Foreground service is started.", Toast.LENGTH_LONG).show();
+                        break;
+                    case ACTION_STOP_FOREGROUND_SERVICE:
+                        stopForegroundService();
+                        Toast.makeText(getApplicationContext(), "Foreground service is stopped.", Toast.LENGTH_LONG).show();
+                        break;
+                    case ACTION_PLAY:
+                        Toast.makeText(getApplicationContext(), "You click Play button.", Toast.LENGTH_LONG).show();
+                        break;
+                    case ACTION_PAUSE:
+                        Toast.makeText(getApplicationContext(), "You click Pause button.", Toast.LENGTH_LONG).show();
+                        break;
+                }
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    /* Used to build and start foreground service. */
+    private void startForegroundService() {
+        Log.d(TAG_FOREGROUND_SERVICE, "Start foreground service.");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel("my_service", "My Background Service");
+        } else {
+
+            // Create notification default intent.
+            Intent intent = new Intent();
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            // Create notification builder.
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+            // Make notification show big text.
+            NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+            bigTextStyle.setBigContentTitle("Music player implemented by foreground service.");
+            bigTextStyle.bigText("Android foreground service is a android service which can run in foreground always, it can be controlled by user via notification.");
+            // Set big text style.
+            builder.setStyle(bigTextStyle);
+
+            builder.setWhen(System.currentTimeMillis());
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+            Bitmap largeIconBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background);
+            builder.setLargeIcon(largeIconBitmap);
+            // Make the notification max priority.
+            builder.setPriority(Notification.PRIORITY_MAX);
+            // Make head-up notification.
+            builder.setFullScreenIntent(pendingIntent, true);
+
+            // Add Play button intent in notification.
+            Intent playIntent = new Intent(this, MyService.class);
+            playIntent.setAction(ACTION_PLAY);
+            PendingIntent pendingPlayIntent = PendingIntent.getService(this, 0, playIntent, 0);
+            NotificationCompat.Action playAction = new NotificationCompat.Action(android.R.drawable.ic_media_play, "Play", pendingPlayIntent);
+            builder.addAction(playAction);
+
+            // Add Pause button intent in notification.
+            Intent pauseIntent = new Intent(this, MyService.class);
+            pauseIntent.setAction(ACTION_PAUSE);
+            PendingIntent pendingPrevIntent = PendingIntent.getService(this, 0, pauseIntent, 0);
+            NotificationCompat.Action prevAction = new NotificationCompat.Action(android.R.drawable.ic_media_pause, "Pause", pendingPrevIntent);
+            builder.addAction(prevAction);
+
+            // Build the notification.
+            Notification notification = builder.build();
+
+            // Start foreground service.
+            startForeground(1, notification);
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private void createNotificationChannel(String channelId, String channelName) {
+        Intent resultIntent = new Intent(this, MainActivity.class);
+// Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationChannel chan = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("App is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .setContentIntent(resultPendingIntent) //intent
+                .build();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, notificationBuilder.build());
+        startForeground(1, notification);
+    }
+
+
+    private void stopForegroundService() {
+        Log.d(TAG_FOREGROUND_SERVICE, "Stop foreground service.");
+
+        // Stop foreground service and remove the notification.
+        stopForeground(true);
+
+        // Stop the foreground service.
+        stopSelf();
+    }
+}
